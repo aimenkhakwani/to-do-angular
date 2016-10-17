7cab1ad4b2d3c75cfce354ba8d1c69974f796c61
Aimen Khakwani and Molly LeCompte <aimen+molly@>
2016-10-17 14:58:48 -0700

    Complete app

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,32 +1,42 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'my-app',
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
-    <h3 (click)="doStuff(currentTask)" *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
+    <pies></pies>
+    <new-task
+      (newTaskSender)="addTask($event)"
+    ></new-task>
+    <task-list
+      [childTaskList]="masterTaskList"
+      (clickSender)="showDetails($event)"
+     ></task-list>
+    <edit-task
+      [childSelectedTask]="selectedTask"
+      (doneClickedSender)="finishedEditing()"
+    ></edit-task>
   </div>
   `
 })
 
 export class AppComponent {
-  public tasks: Task [] = [
-    new Task("Create To-Do List app", 0),
-    new Task("Learn Kung Fu", 1),
-    new Task("Rewatch LotR", 2),
-    new Task("Do laundry", 3)
+  public masterTaskList: Task[] = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
   ];
-  doStuff(clickedTask: Task) {
-    if(clickedTask.done === true) {
-      alert("This task is complete");
-    } else {
-      alert("This task in NOT complete");
-    }
+  selectedTask: Task = null;
+  showDetails(clickedTask: Task) {
+    this.selectedTask = clickedTask;
   }
-}
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public id: number) {}
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+  addTask(newTaskFromChild: Task) {
+    this.masterTaskList.push(newTaskFromChild);
+  }
 }
